924dff1bcbd786ed55dd97253bfc9560071e2461
Victor Ramon <victor.ramon.777@gmail.com>
2025-05-22 00:14:41 -0300

    adicionando perfis

--- src/components/landingpage/AppPerfis.jsx
+++ src/components/landingpage/AppPerfis.jsx
@@ -10,64 +10,68 @@ import {
   ButtonGroup,
   Image,
   Stack,
+  Link,
 } from "@chakra-ui/react";
+
+import GitImage from '../../assets/icons/github.png'
+import LinkedinImage from '../../assets/icons/linkedin.png'
+import AlissonImagem from '../../assets/alisson.png'
+import IsaacImagem from '../../assets/isaac.png'
+import JulliaImagem from '../../assets/jullia.png'
+import ManuelImagem from '../../assets/manuel.png'
+import MayanImagem from '../../assets/mayan.png'
+import RamonImagem from '../../assets/ramon.png'
+import YasminImagem from '../../assets/yasmin.png'
 
 const dataCard = [
   {
-    image:
-      "https://images.unsplash.com/photo-1667489022797-ab608913feeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60",
-    title: "Nome do Desenvolvedor",
-    description: "Descrição do desenvolvedor",
-    linkGithub: "linkGithub",
-    linkLinkedin: "linkLinkedin",
+    image: AlissonImagem,
+    title: "Alisson Sebastian Vieira Sartori ",
+    description: "Desenvolvedor Full-Stack",
+    linkGithub: "https://github.com/alissonsartori",
+    linkLinkedin: "https://www.linkedin.com/in/alisson-sebastian-sartori/",
   },
   {
-    image:
-      "https://images.unsplash.com/photo-1667489022797-ab608913feeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60",
-    title: "Nome do Desenvolvedor",
-    description: "Descrição do desenvolvedor",
-    linkGithub: "linkGithub",
-    linkLinkedin: "linkLinkedin",
+    image: IsaacImagem,
+    title: "Isaac Rodrigues Pereira",
+    description: "Desenvolvedor Front-end",
+    linkGithub: "https://github.com/IsaacPow",
+    linkLinkedin: "https://www.linkedin.com/in/isaac-rodrigues25/",
   },
   {
-    image:
-      "https://images.unsplash.com/photo-1667489022797-ab608913feeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60",
-    title: "Nome do Desenvolvedor",
-    description: "Descrição do desenvolvedor",
-    linkGithub: "linkGithub",
-    linkLinkedin: "linkLinkedin",
+    image: JulliaImagem,
+    title: "Jullia Kathelyn dos Santos Correia",
+    description: "SCRUM Master e Desenvolvedora Full Stack  ",
+    linkGithub: "https://github.com/julliakathelyn",
+    linkLinkedin: "https://www.linkedin.com/in/jullia-kathelyn/",
   },
   {
-    image:
-      "https://images.unsplash.com/photo-1667489022797-ab608913feeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60",
-    title: "Nome do Desenvolvedor",
-    description: "Descrição do desenvolvedor",
+    image: ManuelImagem,
+    title: "Manoel Pires Cobra",
+    description: "UI/UX Designer",
     linkGithub: "linkGithub",
-    linkLinkedin: "linkLinkedin",
+    linkLinkedin: "https://www.linkedin.com/in/manoel-cobra-a190b4253/",
   },
   {
-    image:
-      "https://images.unsplash.com/photo-1667489022797-ab608913feeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60",
-    title: "Nome do Desenvolvedor",
-    description: "Descrição do desenvolvedor",
-    linkGithub: "linkGithub",
-    linkLinkedin: "linkLinkedin",
+    image: MayanImagem,
+    title: "Mayan Santos do Nascimento",
+    description: "Producter Owner",
+    linkGithub: "https://github.com/MayanGit",
+    linkLinkedin: "https://www.linkedin.com/in/mayan-santos27/",
   },
   {
-    image:
-      "https://images.unsplash.com/photo-1667489022797-ab608913feeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60",
-    title: "Nome do Desenvolvedor",
-    description: "Descrição do desenvolvedor",
-    linkGithub: "linkGithub",
-    linkLinkedin: "linkLinkedin",
+    image: RamonImagem,
+    title: "Victor Ramon Dias e Silva",
+    description: "Desenvolvedor Front-end",
+    linkGithub: "https://github.com/vctramon",
+    linkLinkedin: "https://www.linkedin.com/in/victor-ramon-vr/",
   },
   {
-    image:
-      "https://images.unsplash.com/photo-1667489022797-ab608913feeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60",
-    title: "Nome do Desenvolvedor",
-    description: "Descrição do desenvolvedor",
-    linkGithub: "linkGithub",
-    linkLinkedin: "linkLinkedin",
+    image: YasminImagem,
+    title: "Yasmin Araujo Santos",
+    description: "Desenvolvedora Back-end",
+    linkGithub: "https://github.com/yaslopesyweb",
+    linkLinkedin: "https://www.linkedin.com/in/yasmin-lopes-devsecops/",
   },
 ];
 
@@ -89,8 +93,14 @@ const AppPerfis = () => {
       >
         {dataCard.map((item, index) => (
           <Card role="region">
-            <SimpleGrid id="index" key={index} columns={2} spacing={10}>
+            <SimpleGrid 
+            id="index" 
+            key={index} 
+            columns={2} 
+            spacing={10}
+            >
               <Image
+                borderRadius={'10px'}
                 objectFit="cover"
                 boxSize="100%"
                 src={item.image}
@@ -102,10 +112,32 @@ const AppPerfis = () => {
                   <Text>{item.description}</Text>
                 </CardBody>
                 <CardFooter>
-                  <ButtonGroup>
-                    <Button>{item.linkGithub}</Button>
-                    <Button>{item.linkGithub}</Button>
-                  </ButtonGroup>
+                  <Box                
+                    display='flex'
+                    justifyContent='space-around'
+                    // backgroundColor='red'
+                    width='200px'
+                    alignItems='center'
+                  >
+                    <Link href={item.linkGithub} target="blank">
+                      <Image
+                      src={GitImage}
+                      alt="GitHub"
+                      boxSize='64px'
+                      cursor='pointer'
+                      _hover={{transform: 'scale(1.1)', transition: '0.2'}}
+                    />
+                    </Link>
+                     <Link href={item.linkLinkedin} target="blank">
+                      <Image
+                      src={LinkedinImage}
+                      alt="GitHub"
+                      boxSize='64px'
+                      cursor='pointer'
+                      _hover={{transform: 'scale(1.1)', transition: '0.2'}}
+                    />
+                    </Link>
+                  </Box>
                 </CardFooter>
               </Stack>
             </SimpleGrid>
